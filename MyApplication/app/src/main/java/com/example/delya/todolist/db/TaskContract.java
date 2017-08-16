b26bce0f36bf389c01033bed8fa80a0cc55e7e28
egdeliya <egdeliya@mail.ru>
2017-08-16 21:48:18 +0300

    Todo list

---
+++ MyApplication/app/src/main/java/com/example/delya/todolist/db/TaskContract.java
@@ -0,0 +1,18 @@
+package com.example.delya.todolist.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Delya on 16.08.2017.
+ */
+
+public class TaskContract {
+    public static final String DB_NAME = "com.delya.todolist.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
